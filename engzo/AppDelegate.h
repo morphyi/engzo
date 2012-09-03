a174455eb20457dd70cd5434e89c50393e03e083
daCapricorn <capricorn0113@gmail.com>
2012-09-03 22:03:45 +0800

    init project

---
+++ engzo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  engzo
+//
+//  Created by Capricorn on 12-9-3.
+//  Copyright (c) 2012年 engzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ engzo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  engzo
+//
+//  Created by Capricorn on 12-9-3.
+//  Copyright (c) 2012年 engzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
