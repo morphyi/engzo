8453bd5989894c8abf6633a0111137587d6ee50d
daCapricorn <capricorn0113@gmail.com>
2012-09-04 11:48:51 +0800

    add UserSelectController

---
+++ engzo/UserSelectController.h
@@ -0,0 +1,13 @@
+//
+//  UserSelectController.h
+//  engzo
+//
+//  Created by Capricorn on 12-9-4.
+//  Copyright (c) 2012年 engzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserSelectController : UIViewController
+
+@end
---
+++ engzo/UserSelectController.m
@@ -0,0 +1,43 @@
+//
+//  UserSelectController.m
+//  engzo
+//
+//  Created by Capricorn on 12-9-4.
+//  Copyright (c) 2012年 engzo. All rights reserved.
+//
+
+#import "UserSelectController.h"
+
+@interface UserSelectController ()
+
+@end
+
+@implementation UserSelectController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
